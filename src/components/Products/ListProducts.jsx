10f29f88aa177110be8287d11c8d47911cb81ba9
gabyakd2 <gabrielsal00@hotmail.com>
2023-02-28 18:14:26 -0300

    filtro armado

--- src/components/Products/ListProducts.jsx
+++ src/components/Products/ListProducts.jsx
@@ -1,13 +1,34 @@
-import React from "react";
-import { products } from "../../mocks/products.json";
+import React, { useState } from "react";
+import { products as initialProductos } from "../../mocks/products.json";
 import CardProduct from "../CardProduct/CardProduct.jsx";
 import style from './ListProduct.module.css';
 
 export default function ListProducts() {
+
+  const [products] = useState(initialProductos)
+  const [ filters, setFilters ] = useState({
+    category: 'all',
+    minPrice: 0
+  })
+
+  const filterProducts = (products) => {
+    // console.log(products)
+    return products.filter(product => {
+      return (
+        product.price >= filters.minPrice && (
+          filters.category === 'all' || //si no es all pasa a la otra condicion, 
+          product.category === filters.category //sirve para filtrar por categoria
+        )
+      )
+    })
+  }
+
+  const filteredProducts = filterProducts(products)
+
   return (
     <main className={style.products}>
-        {products.map(({ id, title, description, price, brand, thumbnail }) => (
-          <div className={`my-5 ${style.liProduct}`} id={id}>
+        {filteredProducts.map(({ id, title, description, price, brand, thumbnail }) => (
+          <div className={`my-5 ${style.liProduct}`} key={id}>
             <CardProduct
               title={title}
               description={description}
